bd8786f10c77bfdff7c185a197290eb3efc97c00
Kevin <kevin.strebel@yahoo.com>
2022-07-14 15:39:40 -0700

    Fixed some of the interface and added a operate function

--- script.js
+++ script.js
@@ -13,5 +13,15 @@ function Multiply(a, b){
 }
 
 function Divide(a, b) {
+    if (b === 0) return 0;
     return a / b;
+}
+
+function Operate(a, operator, b){
+    switch(operator){
+        case "-": return Subtract(a, b); break;
+        case "+": return Add(a, b); break;
+        case "x": return Multiply(a, b); break;
+        case "/": return Divide(a, b); break;
+    }
 }
