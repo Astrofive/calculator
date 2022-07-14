27929e02427bbe1e094b7e3f757c3632296ad825
Kevin <kevin.strebel@yahoo.com>
2022-07-13 22:06:28 -0700

    Added starting core, still need to align and format buttons. No JS started yet

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("hello");
+
+function Add(a, b){
+    return a + b;
+}
+
+function Subtract(a, b){
+    return a - b;
+}
+
+function Multiply(a, b){
+    return a * b;
+}
+
+function Divide(a, b) {
+    return a / b;
+}
